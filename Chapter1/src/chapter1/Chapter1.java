b2b92e665dc649f91b34050a312e947404c674f6
Eric Charnesky <eric.charnesky@careevolution.com>
2022-05-10 01:28:55 -0400

    chapter 1 examples - here's my lab introduction

---
+++ Chapter1/src/chapter1/Chapter1.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Chapter1 {
+    public static void main(String[] args) {
+        System.out.println("My partner's name is Eric");
+        System.out.println("Eric is taking the course because he's the prof");
+        System.out.println("I've been programming professional for 15 years");
+        System.out.println("I like to play Dungeons and Dragons");
+        System.out.println("We're planning to adopt baby #7 sometime in July");
+        System.out.println("I am training for my 9th marathon, this October, the Freep international");
+    }
+}
