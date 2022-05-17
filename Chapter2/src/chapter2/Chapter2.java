273070a744813d6861a68f571cc6d09b4dc47d6c
Eric Charnesky <eric.charnesky@careevolution.com>
2022-05-17 01:36:49 -0400

    chapter 2 - arithmetic and Strings

--- Chapter2/src/chapter2/Chapter2.java
+++ Chapter2/src/chapter2/Chapter2.java
@@ -38,5 +38,69 @@ public class Chapter2 {
         System.out.println("Number with a decmial's value is: " + numberWithADecimal);
 
         System.out.println("Your number is:" + yourNumber);
+
+        // Please Excuse My Dear Aunt Sally
+        // Parenthesis Exponents ( Multiplication Division ) ( Addition Subtraction )
+
+        System.out.println("What is 5 - 2 + 3");
+        System.out.println(5 - 2 + 3);
+        System.out.println("95% of people can't figure this one out: 9 / 3 * 3 + 2");
+        System.out.println(9 / 3 * 3 + 2);
+
+        myFavoriteNumber = 10 + 2;
+        myFavoriteNumber = myFavoriteNumber + 10;
+        // combined assignment operator
+        myFavoriteNumber += 10;
+
+        // not a combined assignment operator - this will set the value to Positive 10
+        myFavoriteNumber =+ 10;
+
+        myFavoriteNumber = yourNumber * 3 + 6;
+
+        System.out.println("My favorite number is now: " + myFavoriteNumber);
+
+        // integer division gives Integer results - % is the modulus operator, gives the remainder
+        System.out.println("5 / 2 == " + 5 / 2 + " with a reminder of: " + 5 % 2);
+
+        // to get a double result from division, one or both need to be a double
+        double answer = 5.0 / 2;
+
+        System.out.println("5 / 2 with a decimal result is: " + answer);
+
+        // to treat a value as a double, you can 'cast' it as double with (double)
+        double myFavoriteNumberDividedByYourFavoriteNumber = (double)myFavoriteNumber / yourNumber;
+
+        System.out.println(myFavoriteNumberDividedByYourFavoriteNumber);
+
+
+        String firstName = "Eric";
+        String lastName = "Charnesky";
+
+        String fullName = firstName + " " + lastName;
+
+        // we're not "adding" we're concatenating
+        System.out.println(fullName);
+
+        String ericsNameAndFavoriteNumber = fullName + " " + myFavoriteNumber;
+
+        System.out.println(ericsNameAndFavoriteNumber);
+
+        // the backslash is the 'escape' character - means the next letter is special
+        System.out.println("My friend O'Shane said \"Hey Eric!\"");
+
+        // \n new line - goes down a line
+        System.out.println("New Line == \n \\n");
+
+
+        // \t is a tab
+        System.out.println("My Coffee Menu:");
+        System.out.println("$1\t\tCoffee");
+        System.out.println("$1.5\tEspresso");
+        System.out.println("$2\t\tLatte");
+
+        System.out.println("My Coffee Menu:");
+        System.out.println("$1    Coffee");
+        System.out.println("$1.5  Espresso");
+        System.out.println("$2    Latte");
     }
 }
