99efabab13ca7f8af0f015f8df5c61a566de8315
Eric Charnesky <eric.charnesky@careevolution.com>
2022-05-25 02:08:02 -0400

    variable declaration, assignment, and operators

--- Week3/src/week3/Main.java
+++ Week3/src/week3/Main.java
@@ -45,5 +45,54 @@ public class Main {
         boolean trueOrFalseValue = true;
         boolean thisIsFalse = false;
 
+        // declare a value
+        double myNumber;
+
+        // assigning a value - AKA initializing
+        myNumber = 42.0;
+
+        // legal, but makes Eric sad
+        int myFirstNumber, mySecondNumber, myThirdNumber;
+
+        // again, still legal, still sad
+        double firstDouble = 1.2, secondDouble = 2.4, thirdDouble = 3.6;
+
+        // it doesn't work like you might think
+        //int anotherNumber, moreNumbers, lastNumber = 10;
+
+        //System.out.println(anotherNumber);
+        //System.out.println(moreNumbers);
+        //System.out.println(lastNumber);
+
+        // final means you can set the value once and only once
+        final double SALES_TAX_RATE = .06;
+
+        // prevents you from changing it
+        // SALES_TAX_RATE = .7;
+
+        // don't change the sales tax rate
+        //salesTaxRate = .7;
+
+        double result = 9 / 3 * 3 + 2;
+
+        // integer division - int divided an int, results in an int
+        double quotient = 5 / 2; // you'd expect 2.5 - but you get 2
+        int remainder = 5 % 2; // modulus - remainder
+        System.out.println("5/2 == " + quotient + " remainder: " + remainder);
+
+        // to get a double result, at least one of the operands needs to be a double
+        double betterQuotient = 5.0 / 2;
+        System.out.println("5.0/2 == " + betterQuotient);
+
+        int myFavoriteNumber = 42;
+        myFavoriteNumber = myFavoriteNumber + 10;
+        myFavoriteNumber += 10; // combined assignment operator
+
+        myFavoriteNumber += 1;
+        myFavoriteNumber++; // increment operator
+
+        myFavoriteNumber -= 1;
+        myFavoriteNumber--; // decrement operator
+
     }
 }
