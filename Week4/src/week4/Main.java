644155db2ca6628c7ef4163a4fdfe063ea2f3cd3
Eric Charnesky <eric.charnesky@careevolution.com>
2022-06-01 01:50:56 -0400

    swtiches

--- Week4/src/week4/Main.java
+++ Week4/src/week4/Main.java
@@ -17,9 +17,9 @@ public class Main {
         // false && true == false
         // false && false == false
 
-        if ( age < 25 && gender.equalsIgnoreCase("m")){
+        if (age < 25 && gender.equalsIgnoreCase("m")) {
             System.out.println("Your insurance rate is $250/month");
-        } else if ( age < 25 ){
+        } else if (age < 25) {
             System.out.println("Your insurance rate is $200/month");
         } else {
             System.out.println("Your insurance rate is $150/month");
@@ -37,11 +37,11 @@ public class Main {
         System.out.println("Do you have any previous bankruptcies? (y/n");
         String previousBankruptcies = keyboard.nextLine();
 
-        if ( annualIncome > 1_000_000 ||
-                ( annualIncome > 50_000 &&
-                        previousBankruptcies.equalsIgnoreCase("n")) ){
+        if (annualIncome > 1_000_000 ||
+                (annualIncome > 50_000 &&
+                        previousBankruptcies.equalsIgnoreCase("n"))) {
             System.out.println("Here's your loan");
-        } else{
+        } else {
             System.out.println("No loan for you!");
         }
 
@@ -52,10 +52,65 @@ public class Main {
         String playAgain = keyboard.nextLine();
 
         // while play again is NOT equal to N
-        while ( !playAgain.equalsIgnoreCase("n")){
+        while (!playAgain.equalsIgnoreCase("n")) {
             System.out.println("Do you want to play again? (y/n)");
             playAgain = keyboard.nextLine();
         }
 
+        System.out.println("Enter your favorite color");
+        String favoriteColor = keyboard.nextLine().toLowerCase();
+
+        if (favoriteColor.equalsIgnoreCase("blue")) {
+            System.out.println("That's my favorite too!");
+        } else if (favoriteColor.equalsIgnoreCase("red")) {
+            System.out.println("Red is my oldest son's favorite");
+        } else if (favoriteColor.equalsIgnoreCase("purple")) {
+            System.out.println("That is my middle daughter's favorite color!");
+        } else if (favoriteColor.equalsIgnoreCase("yellow")) {
+            System.out.println("That is my oldest daughter's favorite color!");
+        } else {
+            System.out.println("I don't have a kid who likes that color");
+        }
+
+        switch (favoriteColor) {
+            case "blue":
+                System.out.println("That's my favorite too!");
+                break; // ends the switch
+            case "red":
+                System.out.println("Red is my oldest son's favorite");
+                break;
+            case "purple":
+                System.out.println("That is my middle daughter's favorite color!");
+                break;
+            case "yellow":
+                System.out.println("That is my oldest daughter's favorite color!");
+                break;
+            default:
+                System.out.println("I don't have a kid who likes that color");
+        }
+
+        System.out.println("Enter your favorite number");
+        int favoriteNumber = Integer.parseInt(keyboard.nextLine());
+
+        if ( favoriteNumber > 0 && favoriteNumber < 10){
+            System.out.println("You like single digits");
+        } else if ( favoriteNumber >= 10 && favoriteNumber < 100 ){
+            System.out.println("You like two digit numbers");
+        } else{
+            System.out.println("You like big numbers");
+        }
+
+        switch (favoriteNumber){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("You like single digits");
+        }
     }
 }
